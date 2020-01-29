a891589e3ddd79c82dcceefd8dafed8f289babca
Jonas <jonasniedermair@gmail.com>
2020-01-29 13:31:44 +0100

    Add widget build

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -8,9 +8,20 @@
 // @remove-on-eject-end
 'use strict';
 
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+// Set enviroment passed by arguments (--prod = production)
+// Default enviroment is development
+const arg = process.argv[2];
+
+switch (arg) {
+  case '--prod':
+    process.env.BABEL_ENV = 'production';
+    process.env.NODE_ENV = 'production';
+    break;
+
+  default:
+    process.env.BABEL_ENV = 'development';
+    process.env.NODE_ENV = 'development';
+}
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -36,6 +47,7 @@ const chalk = require('react-dev-utils/chalk');
 const fs = require('fs-extra');
 const webpack = require('webpack');
 const configFactory = require('../config/webpack.config');
+const configWidgetFactory = require('../config/webpack.widget.config');
 const paths = require('../config/paths');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
@@ -61,6 +73,8 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
 
 // Generate configuration
 const config = configFactory('production');
+const configWidget = configWidgetFactory('production');
+
 
 // We require that you explicitly set browsers and do not fall back to
 // browserslist defaults.
@@ -120,6 +134,8 @@ checkBrowsers(paths.appPath, isInteractive)
         buildFolder,
         useYarn
       );
+
+      buildWidget();
     },
     err => {
       const tscCompileOnError = process.env.TSC_COMPILE_ON_ERROR === 'true';
@@ -218,6 +234,71 @@ function build(previousFileSizes) {
     });
   });
 }
+
+// Create the build which can then be used inside the widget configurator
+function buildWidget() {
+  // We used to support resolving modules according to `NODE_PATH`.
+  // This now has been deprecated in favor of jsconfig/tsconfig.json
+  // This lets you use absolute paths in imports inside large monorepos:
+  if (process.env.NODE_PATH) {
+    console.log(
+      chalk.yellow(
+        'Setting NODE_PATH to resolve modules absolutely has been deprecated in favor of setting baseUrl in jsconfig.json (or tsconfig.json if you are using TypeScript) and will be removed in a future major release of create-react-app.'
+      )
+    );
+    console.log();
+  }
+
+  console.log('Creating an optimized widget build');
+
+  const compiler = webpack(configWidget);
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      let messages;
+      if (err) {
+        if (!err.message) {
+          return reject(err);
+        }
+        messages = formatWebpackMessages({
+          errors: [err.message],
+          warnings: [],
+        });
+      } else {
+        messages = formatWebpackMessages(
+          stats.toJson({ all: false, warnings: true, errors: true })
+        );
+      }
+      if (messages.errors.length) {
+        // Only keep the first error. Others are often indicative
+        // of the same problem, but confuse the reader with noise.
+        if (messages.errors.length > 1) {
+          messages.errors.length = 1;
+        }
+        return reject(new Error(messages.errors.join('\n\n')));
+      }
+      if (
+        process.env.CI &&
+        (typeof process.env.CI !== 'string' ||
+          process.env.CI.toLowerCase() !== 'false') &&
+        messages.warnings.length
+      ) {
+        console.log(
+          chalk.yellow(
+            '\nTreating warnings as errors because process.env.CI = true.\n' +
+              'Most CI servers set it automatically.\n'
+          )
+        );
+        return reject(new Error(messages.warnings.join('\n\n')));
+      }
+
+      return resolve({
+        stats,
+        warnings: messages.warnings,
+      });
+    });
+  });
+}
+
 
 function copyPublicFolder() {
   fs.copySync(paths.appPublic, paths.appBuild, {
